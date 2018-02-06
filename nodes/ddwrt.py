f20e9073b739b8853dfc8a3ae017cd29cfd4a4ea
André Filipe Silva <andre.silva@introsys.eu>
2018-02-06 16:13:46 +0000

    updated licenses and dependencies

--- nodes/ddwrt.py
+++ nodes/ddwrt.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
+# Software License Agreement
 #
-# Copyright (c) 2008, Willow Garage, Inc.
+# Original work Copyright (c) 2008, Willow Garage, Inc. (BSD License)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
 #
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of the Willow Garage nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above
+#   copyright notice, this list of conditions and the following
+#   disclaimer in the documentation and/or other materials provided
+#   with the distribution.
+# * Neither the name of the Willow Garage nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -30,6 +30,31 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Modified work Copyright 2018 Introsys S.A.
+# Software License Agreement:
+#
+# The software supplied herewith by Introsys - Integration for Robotic
+# Systems (the "Company") for its AGV Mobile Robot is intended
+# and supplied to you, the Company's customer, for use solely and
+# exclusively on or with the AGV Mobile Robot. The software is owned
+# by the  Company and/or its supplier, and is protected under
+# applicable  copyright laws. All rights are reserved.
+# Any use in violation of the foregoing restrictions may subject the
+# user to criminal sanctions under applicable laws, as well as to
+# civil liability for the breach of the terms and conditions of this
+# license.
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
+# WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+# TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+# IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+# CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
 #
 # Revision $Id: gossipbot.py 1013 2008-05-21 01:08:56Z sfkwc $
 
@@ -46,7 +71,6 @@ import rospy
 from wifi_ddwrt.msg import *
 from mechanize import Browser
 from std_msgs.msg import Header
-
 
 import diagnostic_updater
 import diagnostic_msgs
