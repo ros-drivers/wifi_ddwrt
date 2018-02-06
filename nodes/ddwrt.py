38e82cece0f9aa611f0e906eda7b5f5884706981
André Filipe Silva <andre.silva@introsys.eu>
2018-02-06 14:05:40 +0000

    Change the way the node publish information
    - publishes the AP information as a diagnostic message
    - still publishes the SiteSurvey (it is really needed ?? )
    - Tested with the firmware Firmware: DD-WRT v3.0-r34320M kongat (01/02/18)
    - TODO prettify the diagnostic message with the correct names for some values (WIP)

--- nodes/ddwrt.py
+++ nodes/ddwrt.py
@@ -33,27 +33,60 @@
 #
 # Revision $Id: gossipbot.py 1013 2008-05-21 01:08:56Z sfkwc $
 
-import os, sys, string, time, getopt, re
+import os
+import string
+import time
+import getopt
+import re
+import csv
+import gc
 import StringIO
 import rospy
-import diagnostic_msgs
+
 from wifi_ddwrt.msg import *
 from mechanize import Browser
 from std_msgs.msg import Header
-import csv
-import re
-import gc
+
+import diagnostic_updater
+import diagnostic_msgs
 
 
 class WifiAP:
+  """
+    WiFiAP Class
+  """
+  
   def __init__(self, hostname, username, password):
+    """
+    TODO - description
+    :param hostname:
+    :param username:
+    :param password:
+    """
     self.hostname = hostname
     self.username = username
     self.password = password
+    
+    self.essid = ''
+    self.macaddress = ''
+    self.channel = ''
+    self.rate = ''
+    self.tx_power = ''
+    
+    self.ap_ok = False
+    self.last_ex = ' '
+    
+    # List of interfaces with the following structure
+    # client = {'macaddr': None, 'signal': None, 'noise': None, 'snr': None, 'quality': None, 'nt_devices': []}
+    self.interfaces = {}
   
   # ----------------------------------------------------------------------------------------------------------------- #
   
   def newBrowser(self):
+    """
+    Creates a Browser object responsible for access the DDWRT page and gather the necessary information
+    :return:
+    """
     # Create new browsers all the time because its data structures grow
     # unboundedly (texas#135)
     br = Browser()
@@ -64,163 +97,255 @@ class WifiAP:
   # ----------------------------------------------------------------------------------------------------------------- #
   
   def fetchSiteSurvey(self):
-    
-    url = "http://%s/Site_Survey.asp" % self.hostname
-    
-    response = self.newBrowser().open(url)
-    
-    body = response.read()
-    
-    print(body)
-    
-    # make sure that we put a stamp on things
-    header = Header()
-    header.stamp = rospy.Time.now()
-    networks = []
-    survey = SiteSurvey(header, networks)
-    
-    lines = body.split("\n")
-    for i in range(len(lines)):
-      if lines[i].startswith("var table = "):
-        break
-    
-    aplines = []
-    for j in range(i + 1, len(lines)):
-      if lines[j].startswith(");"): break
-      line = lines[j].strip()
-      if not line: continue
-      if line[0] == ",": line = line[1:]
-      
-      aplines.append(line)
-    
-    fp = StringIO.StringIO(string.join(aplines, '\n'))
-    reader = csv.reader(fp)
-    for row in reader:
-      essid = row[0]
-      macattr = row[2]
-      channel = int(row[3])
-      rssi = int(row[4])
-      noise = int(row[5])
-      beacon = int(row[6])
-      
-      network = Network(macattr, essid, channel, rssi, noise, beacon)
-      survey.networks.append(network)
+    """
+    Fetch the survey info from the DDWRT router
+    :return: A list of networks found by the survey (Network.msg)
+    """
+    try:
+      url = 'http://{0}/Site_Survey.asp'.format(self.hostname)
+      response = self.newBrowser().open(url)
+      body = response.read()
+      # make sure that we put a stamp on things
+      header = Header()
+      header.stamp = rospy.Time.now()
+      networks = []
+      survey = SiteSurvey(header, networks)
+      lines = body.split('\n')
+      for i in range(len(lines)):
+        if lines[i].startswith('var table = '):
+          break
+      aplines = []
+      for j in range(i + 1, len(lines)):
+        if lines[j].startswith(');'):
+          break
+        line = lines[j].strip()
+        if not line:
+          continue
+        if line[0] == ',':
+          line = line[1:]
+        aplines.append(line)
+      fp = StringIO.StringIO(string.join(aplines, '\n'))
+      reader = csv.reader(fp)
+      for row in reader:
+        essid = row[0]
+        macattr = row[2]
+        channel = int(row[3])
+        rssi = int(row[4])
+        noise = int(row[5])
+        beacon = int(row[6])
+        network = Network(macattr, essid, channel, rssi, noise, beacon)
+        survey.networks.append(network)
+    except Exception as ex:
+      rospy.logwarn('Error fetching data from the Site Survey, with the following message: {0}'.format(ex))
     return survey
   
   # ----------------------------------------------------------------------------------------------------------------- #
   
   def fetchBandwidthStats(self, interface):
-    url = "http://%s/fetchif.cgi?%s" % (self.hostname, interface)
-    response = self.newBrowser().open(url)
-    body = response.read()
-    
-    lines = body.split("\n")
-    
-    if len(lines) > 1:
-      line = lines[1].strip()
-      iparts = line.split(":", 1)
-      parts = iparts[1].split()
-      
-      print
-      "interface ->  {0}".format(parts)
+    """
+    TODO - add description
+    :param interface:
+    :return:
+    """
+    parts = []
+    try:
+      url = 'http://{0}/fetchif.cgi?{1}'.format(self.hostname, interface)
+      response = self.newBrowser().open(url)
+      body = response.read()
+      lines = body.split('\n')
+      if len(lines) > 1:
+        line = lines[1].strip()
+        iparts = line.split(':', 1)
+        parts = iparts[1].split()
+    except Exception as ex:
+      last_ex = 'Error fetching data from the interface {0} with the error {1}'.format(interface, ex)
+      rospy.logwarn(last_ex)
+      self.ap_ok = False
+    return parts
   
   # ----------------------------------------------------------------------------------------------------------------- #
   
   def fetchCurrentAP(self):
-    url = "http://%s/Status_Wireless.live.asp" % self.hostname
-    response = self.newBrowser().open(url)
-    body = response.read()
-    
-    lines = re.findall(r'\{(.*?)\}', body)
-    
-    print(lines)
-    
-    d = {}
-    line = None
-    for line in lines:
-      parts = line.split("::", 1)
-      if len(parts) == 2:
-        d[parts[0]] = parts[1]
-    
-    essid = d.get('wl_ssid', '')
-    wl_channel = d.get('wl_channel', '').split()[0]
-    channel = int(wl_channel)
-    rate = d.get('wl_rate', '')
-    
-    signal = None
-    noise = None
-    snr = None
-    quality = None
-    
-    tx_power = d.get('wl_xmit', '')
-    
-    active_wireless = d.get('active_wireless', None)
-    if active_wireless:
-      active_wireless = active_wireless.replace("'", "")
-      l_devices = active_wireless.split(',')
-      devices = zip(*[iter(l_devices)] * 10)
+    """
+    TODO - add description
+    :return:
+    """
+    try:
+      url = 'http://{0}/Status_Wireless.live.asp'.format(self.hostname)
+      response = self.newBrowser().open(url)
+      body = response.read()
+      lines = re.findall(r'\{(.*?)\}', body)
+      d = {}  # dictionary
+      line = None
+      for line in lines:
+        parts = line.split("::", 1)
+        if len(parts) == 2:
+          d[parts[0]] = parts[1]
       
-      for device in devices:
-        macaddr = device[0]
-        interface = device[1]
+      self.essid = d.get('wl_ssid', '')
+      self.macaddress = d.get('wl_mac', '')
+      self.channel = int(d.get('wl_channel', '').split()[0])
+      self.rate = d.get('wl_rate', '')
+      self.tx_power = d.get('wl_xmit', '')
+      
+      active_wireless = d.get('active_wireless', None)
+      
+      self.interfaces = {}
+      if active_wireless:
+        active_wireless = active_wireless.replace("'", '')  # we want to remove the apostrophe
+        l_devices = active_wireless.split(',')
+        clients = zip(*[iter(l_devices)] * 10)
         
-        # Netgear R7800devices = zip(*[iter(active_wireless)] * 10)
-        if len(device) == 10:
-          signal = int(device[6])
-          noise = int(device[7])
-          snr = int(device[8])
-          quality = int(device[9]) / 10
-        
-        self.fetchBandwidthStats(interface)
+        # clean all the information about the interfaces
+        self.interfaces = {}
+        # grab all the client interfaces an their status
+        for client in clients:
+          macaddr = client[0]
+          interface = client[1]
+          signal = 9999
+          noise = 9999
+          snr = 9999
+          quality = 9999
+          try:
+            # netgear R7800
+            if len(client) == 10:
+              signal = int(client[6])
+              noise = int(client[7])
+              snr = int(client[8])
+              quality = int(client[9]) / 10
+            # old devices (Not sure if this is really needed but maintained for compatibility)
+            elif len(client) == 7:
+              signal = int(client[4])
+              noise = int(client[5])
+              snr = int(client[6])
+              quality = signal * 1.24 + 116
+            else:
+              signal = int(client[5])
+              noise = int(client[6])
+              snr = int(client[7])
+              quality = int(client[8]) / 10
+          except Exception as ex:
+            last_ex = 'Unable to read data from interface {0} with error: {1}'.format(interface, ex)
+            rospy.logwarn(last_ex)
+            self.ap_ok = False
+          
+          nt_devices = self.fetchBandwidthStats(interface)
+          self.interfaces.setdefault(interface, []).append({'macaddr': macaddr,
+                                                            'signal': signal,
+                                                            'noise': noise,
+                                                            'snr': snr,
+                                                            'quality': quality,
+                                                            'nt_devices': nt_devices})
+          self.last_ex = ''
+          self.ap_ok = True
+    except Exception as ex:
+      last_ex = 'Unable to access the AP at {0}. Error: {1}'.format(self.hostname, ex)
+      rospy.logwarn(last_ex)
+      self.ap_ok = False
+  
+  # ------------------------------------------------------------------------------------------------------------------- #
+  
+  def produce_diagnostics(self, stat):
+    
+    """
+    This creates a diagnostic message an sent it to the Diagnostic Aggregator
+    :param stat:
+    :return:
+    """
+    stat.clearSummary()
+    try:
+      if self.ap_ok:
+        stat.summary(diagnostic_msgs.msg.DiagnosticStatus.OK, 'AP {0} [{1}] working OK'.format(self.essid, self.hostname))
+      else:
+        stat.summary(diagnostic_msgs.msg.DiagnosticStatus.ERROR, self.last_ex)
+      
+      stat.add('essid', self.essid)
+      stat.add('channel', self.channel)
+      stat.add('rate', self.rate)
+      stat.add('tx_power', self.tx_power)
+      
+      for it in self.interfaces.keys():
+        stat.add(it, self.interfaces[it])
+    
+    except Exception as ex:
+      error = 'Unable to access the AP at {0}. Error: {1}'.format(self.hostname, ex)
+      rospy.logwarn(error)
+      stat.summary(diagnostic_msgs.msg.DiagnosticStatus.ERROR, error)
+    finally:
+      return stat
+  
+  # ------------------------------------------------------------------------------------------------------------------ #
+  
+  def check_signal(self, stat):
+    pass
+  
+  # ------------------------------------------------------------------------------------------------------------------ #
+  
+  def check_throughput(self, stat):
+    pass
+  
+  # ------------------------------------------------------------------------------------------------------------------ #
 
 
-# ------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
 
 def loop():
+  """
+  TODO - add description
+  :return:
+  """
   rospy.init_node("wifi_ddwrt")
   
   router_ip = rospy.get_param('~hostname', '192.168.1.1')
   username = rospy.get_param('~username', 'admin')
   password = rospy.get_param('~password', 'admin')
-  
-  ap = WifiAP(router_ip, username, password)
+  node_rate = rospy.get_param('~node_rate', 0.5)
+  diag_rate = rospy.get_param('~diag_rate', 1)
+  diag_agg_topic = rospy.get_param('~diag_agg_topic', 'wifi_ap')
+  site_survey_update = rospy.get_param('~site_survey_update', 120)
+  hw_id = rospy.get_param('~hd_id', 'RouterXPTO')
   
   pub1 = rospy.Publisher("ddwrt/sitesurvey", SiteSurvey, queue_size=1)
   
-  r = rospy.Rate(.5)
-  lastTime = 0
-  last_ex = ''
+  ap = WifiAP(router_ip, username, password)
+  
+  updater = diagnostic_updater.Updater()
+  updater.add(diag_agg_topic, ap.produce_diagnostics)
+  updater.setHardwareID(hw_id)
+  updater.period = diag_rate
+  
+  r = rospy.Rate(node_rate)
+  last_time = 0
+  
   while not rospy.is_shutdown():
     # Needed because mechanize leaves data structures that the GC sees as uncollectable (texas#135)
     breakUpTrash()
-    try:
-      '''
-      if time.time() - lastTime > 60:
-        try:
-          survey = ap.fetchSiteSurvey()
-          pub1.publish(survey)
-          lastTime = time.time()
-        except Exception as e:
-          rospy.logwarn("Caught exception on [fetchSiteSurvey]: %s" % e)
-      '''
-      ap.fetchCurrentAP()
-      
-      '''
-      if node:
-        pub2.publish(node)
-      '''
-      
-      last_ex = ''
-    except Exception as e:
-      if e != last_ex:
-        rospy.logwarn("Caught exception [%s]" % e)
-      last_ex = e
+    
+    if time.time() - last_time > site_survey_update:
+      try:
+        survey = ap.fetchSiteSurvey()
+        pub1.publish(survey)
+        last_time = time.time()
+      except Exception as e:
+        rospy.logwarn("Caught exception on [fetchSiteSurvey]: %s" % e)
+    
+    ap.fetchCurrentAP()
+    updater.update()
+    
     r.sleep()
 
 
 # ------------------------------------------------------------------------------------------------------------------- #
 
 def breakUpTrash():
+  """
+  TODO - add description
+  :return:
+  """
+  
   for item in gc.garbage:
     if type(item) == dict:
       for s in item.keys_iter():
@@ -231,6 +356,10 @@ def breakUpTrash():
 # ------------------------------------------------------------------------------------------------------------------- #
 
 def test():
+  """
+  TODO - add description
+  :return:
+  """
   router_ip = rospy.get_param('~hostname', '192.168.1.1')
   username = rospy.get_param('~username', 'admin')
   password = rospy.get_param('~password', 'admin')
@@ -250,6 +379,11 @@ def test():
 # ------------------------------------------------------------------------------------------------------------------- #
 
 def usage(progname):
+  """
+  TODO - add description
+  :param progname:
+  :return:
+  """
   print
   __doc__ % vars()
 
@@ -257,6 +391,13 @@ def usage(progname):
 # ------------------------------------------------------------------------------------------------------------------- #
 
 def main(argv, stdout, environ):
+  """
+  Main execution
+  :param argv:
+  :param stdout:
+  :param environ:
+  :return:
+  """
   progname = argv[0]
   optlist, args = getopt.getopt(argv[1:], "", ["help", "test", ])
   
@@ -278,6 +419,10 @@ def main(argv, stdout, environ):
 
 # ------------------------------------------------------------------------------------------------------------------- #
 
-
 if __name__ == "__main__":
+  """
+  Main
+  """
   main(sys.argv, sys.stdout, os.environ)
+
+# EOF
